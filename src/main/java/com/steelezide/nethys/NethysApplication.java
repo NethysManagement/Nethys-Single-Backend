aacc5b2457ea0e47f6a482bbf7dd526223544df8
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-11 12:02:29 -0500

    first commit

---
+++ src/main/java/com/steelezide/nethys/NethysApplication.java
@@ -0,0 +1,13 @@
+package com.steelezide.nethys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NethysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NethysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steelezide/nethys/NethysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steelezide.nethys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NethysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
