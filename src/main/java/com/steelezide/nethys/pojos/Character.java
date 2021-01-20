3c737342556a21bcfcd3eb72d2619f2f38d8fdae
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-20 14:09:35 -0500

    Removed unused imports

--- src/main/java/com/steelezide/nethys/pojos/Character.java
+++ src/main/java/com/steelezide/nethys/pojos/Character.java
@@ -1,22 +1,16 @@
 package com.steelezide.nethys.pojos;
 
-import java.util.List;
-
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
