5ea16862461c45eac2176fd734ec7b971e8a85d6
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-25 15:46:13 -0500

    Updated dto folder references

--- src/main/java/com/steelezide/nethys/controllers/CharacterController.java
+++ src/main/java/com/steelezide/nethys/controllers/CharacterController.java
@@ -7,7 +7,7 @@ import com.steelezide.nethys.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import Dtos.CharacterDto;
+import dtos.CharacterDto;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
