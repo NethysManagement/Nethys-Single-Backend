5fc8d3430f02a39330899836d244adff3258054f
Michael Zide <michaelczide@gmail.com>
2021-01-25 09:40:44 -0600

    Changed return types to flux and mono objects

--- src/main/java/com/steelezide/nethys/controllers/CharacterController.java
+++ src/main/java/com/steelezide/nethys/controllers/CharacterController.java
@@ -1,17 +1,22 @@
 package com.steelezide.nethys.controllers;
 
-import com.steelezide.nethys.pojos.PlayerCharacter;
+import java.util.List;
+
 import com.steelezide.nethys.services.CharacterService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import Dtos.CharacterDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController("character")
 public class CharacterController {
@@ -19,14 +24,32 @@ public class CharacterController {
     private CharacterService characterService;
 
     @Autowired
-	public CharacterController(CharacterService characterService) {
-		this.characterService = characterService;
-	}
-    
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
     @GetMapping("{id}")
-    public Mono<CharacterDto> getCharacter(@PathVariable("id") int id){
+    public Mono<CharacterDto> getCharacter(@PathVariable("id") int id) {
+        return null;
+    }
+
+    @GetMapping
+    public Flux<List<CharacterDto>> getAllCharacters() {
+        return null;
+    }
+
+    @PostMapping
+    public void createCharacter(@RequestBody CharacterDto characterDto) {
 
     }
-    
-    
+
+    @PutMapping
+    public void updateCharacter(@RequestBody CharacterDto characterDto) {
+
+    }
+
+    @DeleteMapping
+    public void deleteCharacter(@RequestBody CharacterDto characterDto) {
+
+    }
 }
--- src/main/java/com/steelezide/nethys/controllers/ClassController.java
+++ src/main/java/com/steelezide/nethys/controllers/ClassController.java
@@ -1,5 +1,42 @@
 package com.steelezide.nethys.controllers;
 
+import com.steelezide.nethys.pojos.PlayerClass;
+import com.steelezide.nethys.services.ClassService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Dtos.ClassDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController("class")
 public class ClassController {
     
-}
+    private ClassService classService;
+
+    @Autowired
+    public ClassController(ClassService classService) {
+        this.classService = classService;
+    }
+
+    @GetMapping("{name}")
+    public Mono<PlayerClass> getClass(@PathVariable String name){
+        return null;   
+    }
+
+    @GetMapping
+    public Flux<PlayerClass> getAllClasses(){
+        return null;   
+    }
+
+    @PostMapping
+    public void createClass(@RequestBody ClassDto classDto){
+        
+    }
+
+}   
