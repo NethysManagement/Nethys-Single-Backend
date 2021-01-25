e4cdd7f54d6e4b5ed3c37061cf5458106f026f5e
Michael Zide <michaelczide@gmail.com>
2021-01-25 13:52:33 -0600

    SpellController skeleton

--- src/main/java/com/steelezide/nethys/controllers/SpellController.java
+++ src/main/java/com/steelezide/nethys/controllers/SpellController.java
@@ -1,5 +1,44 @@
 package com.steelezide.nethys.controllers;
 
+import com.steelezide.nethys.pojos.Spell;
+import com.steelezide.nethys.services.SpellService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Dtos.SpellDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController("spell")
 public class SpellController {
+    
+    private SpellService spellService;
+
+    @Autowired
+    public SpellController(SpellService spellService) {
+        this.spellService = spellService;
+    }
+
+    @GetMapping("{id}")
+    public Mono<Spell> getSpell(@PathVariable int id){
+        return null;   
+    }
+    
+    @GetMapping
+    public Flux<Spell> getAllSpells(){
+        return null;   
+    }
+    
+    @PostMapping
+    public void createSpell(@RequestBody SpellDto spellDto){
+        
+    }
+
     
 }
--- src/main/java/com/steelezide/nethys/services/CharacterService.java
+++ src/main/java/com/steelezide/nethys/services/CharacterService.java
@@ -1,5 +1,8 @@
 package com.steelezide.nethys.services;
 
+import com.steelezide.nethys.pojos.PlayerCharacter;
+
 public interface CharacterService {
-    
+  
+    public PlayerCharacter getCharacter(int id);
 }
