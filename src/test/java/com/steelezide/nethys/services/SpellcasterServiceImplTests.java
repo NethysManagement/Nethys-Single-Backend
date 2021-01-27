38d57bca071ae1753d53a1a11b29944747212597
Conner Bosch <37884875+cbosch1@users.noreply.github.com>
2021-01-27 17:10:01 -0500

    Added basic test structure, tests still needed

---
+++ src/test/java/com/steelezide/nethys/services/SpellcasterServiceImplTests.java
@@ -0,0 +1,36 @@
+package com.steelezide.nethys.services;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpellcasterServiceImplTests {
+    
+    @BeforeAll
+	static void setUpBeforeClass() {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() {
+	}
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+    }
+    
+    @Test
+    public void testExample_OptimalInput() {
+        // public PlayerCharacter getCharacter(int id) {}
+        fail();
+    }
+}
